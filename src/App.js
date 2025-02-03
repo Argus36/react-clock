dea45b174828a8f03921397bbc1b59c60c86090c
Argus36 <artemsolomakha2007@gmail.com>
2025-02-03 05:05:38 +0300

    made a clock and added working hands to it + there is a background but it will be finished

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ import { useState, useEffect } from "react";
 
 function App() {
 
+  let numbers = {
+    num1 : [
+      {val : "1", part : "part1"}, {val : "2", part : "part2"}, {val : "3", part : "part3"}, {val : "4", part : "part4"}, {val : "5", part : "part5"}, {val : "6", part : "part6"}
+    ],
+    num2 : [
+      {val : "7", part : "part7"}, {val : "8", part : "part8"}, {val : "9", part : "part9"}, {val : "10", part : "part10"}, {val : "11", part : "part11"}, {val : "12", part : "part12"}
+    ]
+  }
+
   const [date, setDate] = useState(moment().format("dddd DD MMMM YYYY"));
-  const [time, setTime] = useState(moment().format("HH mm ss"));
+  const [hour, setHour] = useState(Number(moment().format("HH")));
+  const [minute, setMinute] = useState(Number(moment().format("mm")));
+  const [second, setSecond] = useState(Number(moment().format("ss")));
 
   let timeZone = moment.tz.guess();
   moment.tz.setDefault(timeZone);
@@ -16,7 +27,9 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setDate(moment().format("dddd DD MMMM YYYY"));
-      setTime(moment().format("HH:mm:ss"));
+      setHour(Number(moment().format("HH")));
+      setMinute(Number(moment().format("mm")));
+      setSecond(Number(moment().format("ss")));
     }, 250);
 
     return () => {
@@ -28,14 +41,31 @@ function App() {
 
 
 
-
   return (
     <div className="App">
-      <h1>Сегодняшняя дата — {date}</h1>
-      <h1>Время — {time}</h1>
+      <div className="container">
+        <div className="clock">
+          <div className="block">
+            <div className="dot"></div>
+            <div style={{transform: `translate(0%, -95%) rotate(${hour * 30}deg)`}} className="big-arrow"></div>
+            <div style={{transform: `translate(0%, -152.5%) rotate(${minute * 6}deg)`}} className="middle-arrow"></div>
+            <div style={{transform: `translate(10%, -230%) rotate(${second * 6}deg)`}} className="little-arrow"></div>
+            <div className="numbers">
+              {numbers.num1.map((num) =>
+                <p className={num.part}>{num.val}</p>
+              )}
+              {numbers.num2.map((num) =>
+                <p className={num.part}>{num.val}</p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 
 export default App;
