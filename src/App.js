8b71b08f7f12dd9bde5d9b2b4e0416fe99e2998c
Argus36 <artemsolomakha2007@gmail.com>
2025-02-04 19:09:14 +0300

    Made numbers and corrected the hands and also made not numbers but float meaning because of which now the big hour hand is indicated when for example 18:30 between 6 and 7

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import { useState, useEffect } from "react";
 
 function App() {
 
-  let numbers = {
-    num1 : [
-      {val : "1", part : "part1"}, {val : "2", part : "part2"}, {val : "3", part : "part3"}, {val : "4", part : "part4"}, {val : "5", part : "part5"}, {val : "6", part : "part6"}
-    ],
-    num2 : [
-      {val : "7", part : "part7"}, {val : "8", part : "part8"}, {val : "9", part : "part9"}, {val : "10", part : "part10"}, {val : "11", part : "part11"}, {val : "12", part : "part12"}
-    ]
-  }
-
   const [date, setDate] = useState(moment().format("dddd DD MMMM YYYY"));
   const [hour, setHour] = useState(Number(moment().format("HH")));
   const [minute, setMinute] = useState(Number(moment().format("mm")));
@@ -27,9 +18,9 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setDate(moment().format("dddd DD MMMM YYYY"));
-      setHour(Number(moment().format("HH")));
-      setMinute(Number(moment().format("mm")));
       setSecond(Number(moment().format("ss")));
+      setMinute(Number(moment().format("mm")));
+      setHour(Number(moment().format("HH")));
     }, 250);
 
     return () => {
@@ -38,26 +29,15 @@ function App() {
 
   }, []);
 
-
-
-
   return (
     <div className="App">
       <div className="container">
         <div className="clock">
           <div className="block">
             <div className="dot"></div>
-            <div style={{transform: `translate(0%, -95%) rotate(${hour * 30}deg)`}} className="big-arrow"></div>
-            <div style={{transform: `translate(0%, -152.5%) rotate(${minute * 6}deg)`}} className="middle-arrow"></div>
-            <div style={{transform: `translate(10%, -230%) rotate(${second * 6}deg)`}} className="little-arrow"></div>
-            <div className="numbers">
-              {numbers.num1.map((num) =>
-                <p className={num.part}>{num.val}</p>
-              )}
-              {numbers.num2.map((num) =>
-                <p className={num.part}>{num.val}</p>
-              )}
-            </div>
+            <div style={{transform: `translate(-47.5%, -105%) rotate(${hour * 30 + (minute / 100 * 50)}deg)`}} className="big-arrow"></div>
+            <div style={{transform: `translate(-47.5%, -165%) rotate(${minute * 6  + (second / 100 * 10)}deg)`}} className="middle-arrow"></div>
+            <div style={{transform: `translate(-50%, -234%) rotate(${second * 6}deg)`}} className="little-arrow"></div>
           </div>
         </div>
       </div>
